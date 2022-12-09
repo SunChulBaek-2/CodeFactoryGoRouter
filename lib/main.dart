1469012a24c9e4cb7652fea96e27753caeb8960b
x1210x <x1210x@gmail.com>
2022-12-09 09:58:43 +0900

    GoRouter 세팅하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:codefactory_go_router/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(_App());
+  runApp(const _App());
 }
 
 class _App extends StatelessWidget {
   const _App({Key? key}) : super(key: key);
 
+  GoRouter get _router => GoRouter(
+      initialLocation: '/',
+      routes: [
+        GoRoute(path: '/', builder: (context, state) => const HomeScreen())
+      ]);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp.router(
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
     );
   }
 }
